34d4001e91a442392b74f0fb70b07a4a427ed680
Tran Ngoc Duc <caothu91@gmail.com>
2024-05-18 08:45:42 +0700

    map attribute

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2024-05-14 13:34:14
+ * @lastupdate 2024-05-17 15:16:56
  */
 
 namespace Diepxuan\Simba\Models;
@@ -86,6 +86,46 @@ class Product extends AbstractModel
     {
         return Attribute::make(
             get: static fn (mixed $value, array $attributes) => implode('_', [$attributes['ma_cty'], $attributes['ma_vt']]),
+        );
+    }
+
+    /**
+     * Get the Simba  Sku.
+     */
+    protected function sku(): Attribute
+    {
+        return Attribute::make(
+            get: static fn (mixed $value, array $attributes) => $attributes['ma_vt'],
+        );
+    }
+
+    /**
+     * Get the Simba name.
+     */
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: static fn (mixed $value, array $attributes) => $attributes['ten_vt'],
+        );
+    }
+
+    /**
+     * Get the Simba name.
+     */
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: static fn (mixed $value, array $attributes) => $attributes['gia_nt2'],
+        );
+    }
+
+    /**
+     * Get the Simba Category.
+     */
+    protected function category(): Attribute
+    {
+        return Attribute::make(
+            get: static fn (mixed $value, array $attributes) => $attributes['ma_nhvt'],
         );
     }
 
