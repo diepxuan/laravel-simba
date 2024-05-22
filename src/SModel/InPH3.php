ad2f84e89eb37df88f531429e1caa741d114d35d
Tran Ngoc Duc <caothu91@gmail.com>
2024-05-22 17:28:15 +0700

    create InPH3 model

---
+++ src/SModel/InPH3.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2024-05-22 14:07:15
+ */
+
+namespace Diepxuan\Simba\SModel;
+
+use Diepxuan\Simba\Models\Traits\HasCompositePrimaryKey;
+use Illuminate\Database\Eloquent\Model;
+
+class InPH3 extends SModel
+{
+    use HasCompositePrimaryKey;
+
+    public const CREATED_AT = 'cDate';
+    public const UPDATED_AT = 'lDate';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'InPH3';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = ['ma_cty', 'stt_rec'];
+}
